59a3f2248f79768092d3b0cb23fdcafc690f7379
FrodoNihal <frodo.hatschii@yahoo.de>
2013-08-08 17:59:03 +0200

    Den Autor hinzugefügt.

--- Suchalgorithmen/src/Insertionsort.java
+++ Suchalgorithmen/src/Insertionsort.java
@@ -1,29 +1,33 @@
-
+/**
+ * 
+ * @author Maximilian Scholz
+ *
+ */
 public class Insertionsort {
-
-	
-	public static int[] insertionSort(int[] unsortiertesArray){
-		
-		int zwischenvariable = 0;
-		int dreieckselement = 0;
-		
-		for(int i = 1 ; i < unsortiertesArray.length ; ++i){
-			
-			int index2 = 1;			
-			zwischenvariable = unsortiertesArray[i];						
-			
-			if(zwischenvariable<unsortiertesArray[i-1]){
-				
-				while(i>=index2 && zwischenvariable<unsortiertesArray[i-index2] ){
-							
-					dreieckselement = unsortiertesArray[i-index2];
-					unsortiertesArray[i-index2]= zwischenvariable ;
-					unsortiertesArray[i-index2+1]=dreieckselement;
-					++index2;
-				}			
-			}
-		}
-		
-		return unsortiertesArray;
-	}	
+  
+  
+  public static int[] insertionSort(int[] unsortiertesArray){
+    
+    int zwischenvariable = 0;
+    int dreieckselement = 0;
+    
+    for(int i = 1 ; i < unsortiertesArray.length ; ++i){
+      
+      int index2 = 1;     
+      zwischenvariable = unsortiertesArray[i];            
+      
+      if(zwischenvariable<unsortiertesArray[i-1]){
+        
+        while(i>=index2 && zwischenvariable<unsortiertesArray[i-index2] ){
+          
+          dreieckselement = unsortiertesArray[i-index2];
+          unsortiertesArray[i-index2]= zwischenvariable ;
+          unsortiertesArray[i-index2+1]=dreieckselement;
+          ++index2;
+        }     
+      }
+    }
+    
+    return unsortiertesArray;
+  } 
 }
--- Suchalgorithmen/src/Test.java
+++ Suchalgorithmen/src/Test.java
@@ -1,32 +1,36 @@
-
+/**
+ * 
+ * @author Maximilian Scholz
+ *
+ */
 public class Test {
-
-	public static void arrayAusgeben(int[] array){
-		
-		for(int i = 0 ; i < array.length ; ++i){
-			
-			System.out.print("[ "+array[i]+" ] ");			
-		}		
-		System.out.println();
-	}
-	
-	public static void zufälligesBefüllen(int [] array){
-		
-		for(int i = 0 ; i<array.length ; ++i){
-						
-			array[i] = (int) Math.round((Math.random()*100));
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		int[] array = new int[10];
-
-		zufälligesBefüllen(array);
-		arrayAusgeben(array);
-		Insertionsort.insertionSort(array);
-		arrayAusgeben(array);
-	}
-
+  
+  public static void arrayAusgeben(int[] array){
+    
+    for(int i = 0 ; i < array.length ; ++i){
+      
+      System.out.print("[ "+array[i]+" ] ");      
+    }   
+    System.out.println();
+  }
+  
+  public static void zufälligesBefüllen(int [] array){
+    
+    for(int i = 0 ; i<array.length ; ++i){
+      
+      array[i] = (int) Math.round((Math.random()*100));
+    }
+  }
+  
+  
+  public static void main(String[] args) {
+    
+    int[] array = new int[10];
+    
+    zufälligesBefüllen(array);
+    arrayAusgeben(array);
+    Insertionsort.insertionSort(array);
+    arrayAusgeben(array);
+  }
+  
 }
