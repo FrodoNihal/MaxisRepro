f176e628103a2648a0ee366211cae5b571e55a7a
FrodoNihal <frodo.hatschii@yahoo.de>
2013-08-09 13:00:27 +0200

    Quicksort mit eingebettet und in den Test integriert.

---
+++ Suchalgorithmen/src/Quicksort.java
@@ -0,0 +1,39 @@
+
+public class Quicksort {
+	
+	 public static void fnSortHeap(int array[], int arr_ubound){
+		  int i, o;
+		  int lChild, rChild, mChild, root, temp;
+		  root = (arr_ubound-1)/2;
+
+		  for(o = root; o >= 0; o--){
+		  for(i=root;i>=0;i--){
+		  lChild = (2*i)+1;
+		  rChild = (2*i)+2;
+		  if((lChild <= arr_ubound) && (rChild <= arr_ubound)){
+		  if(array[rChild] >= array[lChild])
+		  mChild = rChild;
+		  else
+		  mChild = lChild;
+		  }
+		  else{
+		  if(rChild > arr_ubound)
+		  mChild = lChild;
+		  else
+		  mChild = rChild;
+		  }
+
+		  if(array[i] < array[mChild]){
+		  temp = array[i];
+		  array[i] = array[mChild];
+		  array[mChild] = temp;
+		  }
+		  }
+		  }
+		  temp = array[0];
+		  array[0] = array[arr_ubound];
+		  array[arr_ubound] = temp;
+		  return;
+		  }
+}
+
--- Suchalgorithmen/src/Test.java
+++ Suchalgorithmen/src/Test.java
@@ -1,36 +1,37 @@
-/**
- * 
- * @author Maximilian Scholz
- *
- */
+
 public class Test {
-  
-  public static void arrayAusgeben(int[] array){
-    
-    for(int i = 0 ; i < array.length ; ++i){
-      
-      System.out.print("[ "+array[i]+" ] ");      
-    }   
-    System.out.println();
-  }
-  
-  public static void zufälligesBefüllen(int [] array){
-    
-    for(int i = 0 ; i<array.length ; ++i){
-      
-      array[i] = (int) Math.round((Math.random()*100));
-    }
-  }
-  
-  
-  public static void main(String[] args) {
-    
-    int[] array = new int[10];
-    
-    zufälligesBefüllen(array);
-    arrayAusgeben(array);
-    Insertionsort.insertionSort(array);
-    arrayAusgeben(array);
-  }
-  
+
+	public static void arrayAusgeben(int[] array){
+		
+		for(int i = 0 ; i < array.length ; ++i){
+			
+			System.out.print("[ "+array[i]+" ] ");			
+		}		
+		System.out.println();
+	}
+	
+	public static void zufälligesBefüllen(int [] array){
+		
+		for(int i = 0 ; i<array.length ; ++i){
+						
+			array[i] = (int) Math.round((Math.random()*100));
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		int[] array = new int[10];
+
+		zufälligesBefüllen(array);
+		arrayAusgeben(array);
+		
+		  for(int i=array.length; i>1; i--){
+			  
+			  Quicksort.fnSortHeap(array, i - 1);
+			  }
+		  
+		arrayAusgeben(array);
+	}
+
 }
